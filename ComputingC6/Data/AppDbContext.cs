2bace31ed5d413fdd912b4ef12f6c45f1d34b4af
dhunganapranab <pranab.mune@gmail.com>
2024-03-13 09:34:39 +0545

    Add project files.

---
+++ ComputingC6/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ComputingC6.Models.Student;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComputingC6.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ ComputingC6/Models/Student/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComputingC6.Models.Student
+{
+    public class Student
+    {
+        [Key]
+        public int LMUID { get; set; }
+
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String fypTitle { get; set; }
+
+    }
+}
